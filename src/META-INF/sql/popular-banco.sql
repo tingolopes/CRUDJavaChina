61d45ae1097b742fd9b8b859c799c53a1eca38a4
tingo <tingolopes@gmail.com>
2019-05-15 22:57:53 -0400

    Atualização das libs

--- src/META-INF/sql/popular-banco.sql
+++ src/META-INF/sql/popular-banco.sql
@@ -1,19 +1,10 @@
+INSERT INTO `usuario` (`id`, `login`, `nome`, `senha`) VALUES
+(1, 'admin', 'admin', 'admin');
+
 INSERT INTO `categoria` (`id`, `descricao`, `nome`) VALUES
 (1, 'Gêneros alimentícios', 'Alimentos'),
 (2, 'Roupas diversas', 'Roupa'),
 (3, 'Calçados diversos', 'Calçados');
-
-INSERT INTO `cliente` (`id`, `endereco`, `nome`) VALUES
-(1, 'Rua A', 'Michell'),
-(2, 'Rua B', 'Kleber'),
-(3, 'Rua C', 'Gabriel');
-
-INSERT INTO `itensvenda` (`id`, `precoProduto`, `qtd`, `totalProduto`, `id_produto`, `id_venda`) VALUES
-(1, 59, 4, 236, 1, 1),
-(2, 28, 2, 56, 2, 1),
-(3, 34, 3, 102, 3, 1),
-(4, 45, 2, 90, 1, 2),
-(5, 34, 3, 102, 3, 2);
 
 INSERT INTO `produto` (`id`, `datacadastro`, `nome`, `preco`, `id_categoria`) VALUES
 (1, '2019-04-26', 'Picanha', 45, 1),
@@ -24,9 +15,18 @@ INSERT INTO `produto` (`id`, `datacadastro`, `nome`, `preco`, `id_categoria`) VA
 (6, '2019-04-26', 'Tenis', 340, 3),
 (7, '2019-04-26', 'Chinelo', 23.5, 3);
 
-INSERT INTO `usuario` (`id`, `login`, `nome`, `senha`) VALUES
-(1, 'admin', 'admin', 'admin');
+INSERT INTO `cliente` (`id`, `endereco`, `nome`) VALUES
+(1, 'Rua A', 'Michell'),
+(2, 'Rua B', 'Kleber'),
+(3, 'Rua C', 'Gabriel');
 
 INSERT INTO `venda` (`id`, `datavenda`, `id_cliente`) VALUES
 (1, '2019-04-26', 2),
 (2, '2019-04-26', 2);
+
+INSERT INTO `itensvenda` (`id`, `precoProduto`, `qtd`, `totalProduto`, `id_produto`, `id_venda`) VALUES
+(1, 59, 4, 236, 1, 1),
+(2, 28, 2, 56, 2, 1),
+(3, 34, 3, 102, 3, 1),
+(4, 45, 2, 90, 1, 2),
+(5, 34, 3, 102, 3, 2);
