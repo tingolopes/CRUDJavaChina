04f05f98ee107aec629cfad39238d300724c3639
tingo <tingolopes@gmail.com>
2019-05-26 00:12:15 -0400

    Mais produtos e clientes no banco

--- src/META-INF/sql/popular-banco.sql
+++ src/META-INF/sql/popular-banco.sql
@@ -11,14 +11,30 @@ INSERT INTO `produto` ( `datacadastro`, `nome`, `preco`, `id_categoria`) VALUES
 ( '2019-04-26', 'Alcatra', 28, 1),
 ( '2019-04-26', 'Filé', 34, 1),
 ( '2019-04-26', 'Blusa', 78, 2),
+( '2019-04-26', 'Leite', 78, 1),
+( '2019-04-26', 'Pao', 78, 1),
+( '2019-04-26', 'Ovo', 78, 1),
+( '2019-04-26', 'Presunto', 78, 1),
+( '2019-04-26', 'Queijo', 78, 1),
+( '2019-04-26', 'Blusa', 78, 2),
+( '2019-04-26', 'Meia', 78, 2),
+( '2019-04-26', 'Casaco', 78, 2),
+( '2019-04-26', 'Touca', 78, 2),
+( '2019-04-26', 'Camiseta', 78, 2),
 ( '2019-04-26', 'Calça', 99, 2),
 ( '2019-04-26', 'Tenis', 340, 3),
+( '2019-04-26', 'Cuturno', 340, 3),
+( '2019-04-26', 'Sapato', 340, 3),
 ( '2019-04-26', 'Chinelo', 23.5, 3);
 
 INSERT INTO `cliente` ( `endereco`, `nome`) VALUES
 ('Rua A', 'Michell'),
 ('Rua B', 'Kleber'),
-('Rua C', 'Gabriel');
+('Rua C', 'Lucas'),
+('Rua D', 'Pedro'),
+('Rua E', 'Ze'),
+('Rua F', 'Fernando'),
+('Rua G', 'Joao');
 
 INSERT INTO `venda` ( `datavenda`, `id_cliente`) VALUES
 ('2019-04-26', 2),
